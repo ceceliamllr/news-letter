ec2fa10a736125be110bd6b7fa9038818bf4f019
cecelia miller <cece@cecelias-MacBook-Air.local>
2022-02-15 17:54:44 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  var data= {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/583e108d5d";
+
+  const options = {
+    method: "post" ,
+    auth: "ceceliamllr:2e3ad04db57dac46fbc80a4570d91d67-us14"
+  }
+
+  const request = https.request(url, options, function(response){
+    if (response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html")
+    } else {
+      res.sendFile(__dirname + "/failure.html")
+    }
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+
+  })
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is running on port 3000");
+});
